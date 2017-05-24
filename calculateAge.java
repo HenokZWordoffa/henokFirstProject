b42ffacab419ac230cc291f1ca37f852071e46a9
HenokZWordoffa <henokzwordoffa@gmail.com>
2017-05-24 16:25:54 -0400

    Added Agecalculation files

---
+++ calculateAge.java
@@ -0,0 +1,13 @@
+public class calculateAge{
+	public static void main(String[] args)
+{
+int day,month,year;
+System.out.print("Enter birth day:");
+String day = System.console().readLine();
+System.out.print("Enter birth month:");
+String month = System.console().readLine();
+System.out.print("Enter birth year:");
+String year = System.console().readLine();
+System.out.println(input);
+}//End of main
+}//End of class
